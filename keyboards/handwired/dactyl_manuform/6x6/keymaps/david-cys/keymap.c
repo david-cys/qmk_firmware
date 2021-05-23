ea2e23821b6c9b99f151f6980ee1026fd7032167
David Chua <david.cys@live.com>
2021-05-23 22:45:33 +0800

    Add home mod keys

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/david-cys/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/david-cys/keymap.c
@@ -8,6 +8,21 @@
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+#define HOME_SC RGUI_T(KC_SCLN)
+#define HOME_L RALT_T(KC_L)
+#define HOME_K RCTL_T(KC_K)
+#define HOME_J RSFT_T(KC_J)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_6x6(
@@ -15,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,					KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,
      KC_ESC ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                   KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_BSPC,
      KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,               	KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_MINS,
-     KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,               	KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,
+     KC_LCTL,HOME_A ,HOME_S ,HOME_D ,HOME_F ,KC_G   ,               	KC_H   ,HOME_J ,HOME_K ,HOME_L ,HOME_SC,KC_QUOT,
      KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,                   KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
 					 KC_LBRC,KC_RBRC,							                	  	KC_PLUS,KC_EQL,
 							 KC_BSPC,KC_SPC ,           						KC_ENT, LOWER,
