1e040f2d233babc0c74b6a7293f77dffb76f4137
David Chua <david.cys@live.com>
2021-05-24 22:44:25 +0800

    Add curly brackets to lower layer

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/david-cys/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/david-cys/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                   KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
      _______,_______,_______,_______,_______,KC_LBRC,                   KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
      _______,_______,_______,_______,_______,KC_LPRN,                   KC_RPRN,KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,KC_PIPE,
-     _______,_______,_______,_______,_______,_______,                   _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+     _______,_______,_______,_______,_______,KC_LCBR,                   KC_RCBR, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
 					 RESET  ,KC_PSCR,            										_______, KC_P0,
                              _______,_______,           						_______,_______,
                                      _______,_______,           		_______,_______,
